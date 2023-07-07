362c86c4eb7074f228945b4c7f794a1d1c19a77c
shawky_53 <mostafashawky100000@gmail.com>
2023-07-07 03:16:56 +0300

    create method

--- object/object.js
+++ object/object.js
@@ -1,37 +1,54 @@
-let user = {
-    // property
-    firstName:'Mostafa',
-    lastName:'Shawky',
-    email:'mostafashawky100000@gmail.com',
-    age:26,
-    skills:['html','css','js','php','mysql'],
-    active:false,
-    phoneNumber:{
-        first:'0106547687',
-        second:'0126574687',
-    },
-    address:{
-        Egypt:'Cairo',
-        Uae:'Dubai',
-    },
-    // method
-    isActive:function(){
-        if(user.active === true){
-            return 'you are active';
-        }
-        else{
-            return 'you are not active';
-        }
-    },
-    getAge:function(){
-        if(user.age >= 18)
-        {
-            return 'available';
-        }
-        else{
-            return 'not available'';
-        }
-    },
-};
-console.log(user.isActive())
-console.log(user['getAge']())
+// let user = {
+//     // property
+//     firstName:'Mostafa',
+//     lastName:'Shawky',
+//     email:'mostafashawky100000@gmail.com',
+//     age:26,
+//     skills:['html','css','js','php','mysql'],
+//     active:false,
+//     phoneNumber:{
+//         first:'0106547687',
+//         second:'0126574687',
+//     },
+//     address:{
+//         Egypt:'Cairo',
+//         Uae:'Dubai',
+//     },
+//     // method
+//     isActive:function(){
+//         if(user.active === true){
+//             return 'you are active';
+//         }
+//         else{
+//             return 'you are not active';
+//         }
+//     },
+//     getAge:function(){
+//         if(user.age >= 18)
+//         {
+//             return 'available';
+//         }
+//         else{
+//             return 'not available'';
+//         }
+//     },
+// };
+// console.log(user.isActive())
+// console.log(user['getAge']())
+
+//create object use create method
+// let user = Object.create(null);
+// user.firstName = 'Mostafa';
+// user.lastName = 'Shawky';
+// user.age = 26;
+// user.skills = ['html','css','js','php','mysql'];
+// user.active = false;
+// user.phoneNumber = {
+//     first:'0106547687',
+//     second:'0126574687',
+// };
+// user.address = {
+//     Egypt:'Cairo',
+//     Uae:'Dubai',
+// };
+// console.log(user);
