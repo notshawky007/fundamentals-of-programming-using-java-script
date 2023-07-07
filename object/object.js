d8c720c9b00621bcc3af2b1214e4d243ea01f673
shawky_53 <mostafashawky100000@gmail.com>
2023-07-07 03:34:42 +0300

    this keyword

--- object/object.js
+++ object/object.js
@@ -52,3 +52,42 @@
 //     Uae:'Dubai',
 // };
 // console.log(user);
+
+//use this in Method,function,function in strict mode,arrow function,Event and print what this refer to in each case
+// let user = {
+//     firstName:'Mostafa',
+//     lastName:'Shawky',
+//     fullName:function(){
+//         return `${this.firstName} ${this.lastName}`;
+//     },
+//     skills:['html','css','js','php','mysql'],
+//     active:false,
+//     phoneNumber:{
+//         first:'0106547687',
+//         second:'0126574687',
+//     },
+//     address:{
+//         Egypt:'Cairo',
+//         Uae:'Dubai',
+//     },
+//     isActive:function(){
+//         if(this.active === true){
+//             return 'you are active';
+//         }
+//         else{
+//             return 'you are not active';
+//         }
+//     },
+//     getAge:function(){
+//         if(this.age >= 18)
+//         {
+//             return 'available';
+//         }
+//         else{
+//             return 'not available';
+//         }
+//     },
+// };
+// console.log(user.fullName());
+// console.log(user.isActive());
+// console.log(user.getAge());
