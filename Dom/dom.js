174ada4d6b757606ea890d4db215ca203bd2f86d
shawky_53 <mostafashawky100000@gmail.com>
2023-07-09 04:12:06 +0300

    practice of innerText, innerHTML, textContent

---
+++ Dom/dom.js
@@ -0,0 +1,57 @@
+//get elements using id, class, tag name, querySelector, querySelectorAll,images,links,forms,forms elements
+// let myDiv = document.getElementById('myDiv');
+// console.log(myDiv);
+// let myDivs = document.getElementsByClassName('myDiv');
+// console.log(myDivs);
+// let myDivs2 = document.getElementsByTagName('div');
+// console.log(myDivs2);
+// let myDivs3 = document.querySelectorAll('.myDiv');
+// console.log(myDivs3);
+// let myDivs4 = document.querySelector('.myDiv');
+// console.log(myDivs4);
+// let myImages = document.images;
+// console.log(myImages);
+// let myLinks = document.links;
+// console.log(myLinks);
+// let myForms = document.forms;
+// console.log(myForms);
+// let myForm = document.forms[0];
+// console.log(myForm);
+
+//get element attributes, set element attributes, remove element attributes
+// let myDiv = document.getElementById('myDiv');
+// console.log(myDiv.getAttribute('id'));
+// console.log(myDiv.getAttribute('class'));
+// console.log(myDiv.getAttribute('data-name'));
+// console.log(myDiv.getAttribute('data-job'));
+// console.log(myDiv.getAttribute('data-lang'));
+// myDiv.setAttribute('data-lang','php');
+// console.log(myDiv.getAttribute('data-lang'));
+// myDiv.removeAttribute('data-lang');
+// console.log(myDiv.getAttribute('data-lang'));
+
+//practice of siblings, children, parent, firstChild, lastChild, firstElementChild, lastElementChild, nextSibling, previousSibling, nextElementSibling, previousElementSibling
+// let myDiv = document.getElementById('myDiv');
+// console.log(myDiv);
+// console.log(myDiv.children);
+// console.log(myDiv.children[0]);
+
+// console.log(myDiv.firstElementChild);
+// console.log(myDiv.lastElementChild);
+// console.log(myDiv.nextElementSibling);
+// console.log(myDiv.previousElementSibling);
+
+// console.log(myDiv.firstChild);
+// console.log(myDiv.lastChild);
+// console.log(myDiv.nextSibling);
+// console.log(myDiv.previousSibling);
+
+//practice of innerText, innerHTML, textContent properties
+// let myDiv = document.getElementById('myDiv');
+// console.log(myDiv.innerText);
+// console.log(myDiv.innerHTML);
+// console.log(myDiv.textContent);
+
+// myDiv.innerText = 'Hello World';
+// myDiv.innerHTML = '<h1>Hello World</h1>';
+// myDiv.textContent = 'Hello World';
