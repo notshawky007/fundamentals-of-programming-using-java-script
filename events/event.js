bf19440da8f0f30c0545377e5d1aa1fac37e34ec
shawky_53 <mostafashawky100000@gmail.com>
2023-07-10 03:48:21 +0300

    practice of events onclick and addEventListener

---
+++ events/event.js
@@ -0,0 +1,14 @@
+//practice of events onclick and addEventListener
+// var btn = document.getElementById("btn");
+// btn.onclick = function(){
+//     alert("Hello");
+// }
+// btn.addEventListener("click",function(){
+//     alert("Hello");
+// })
+// btn.addEventListener("click",function(){
+//     alert("Hello Again");
+// })
+// btn.addEventListener("mouseover",function(){
+//     alert("Hello Again");
+// })
