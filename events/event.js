9c8384e8e709ef1713db37783afad6f60407eb14
shawky_53 <mostafashawky100000@gmail.com>
2023-07-10 03:58:30 +0300

    practice of events

--- events/event.js
+++ events/event.js
@@ -12,3 +12,56 @@
 // btn.addEventListener("mouseover",function(){
 //     alert("Hello Again");
 // })
+
+//practice of events mouseover and mouseout, keydown and keyup, focus and blur, change, submit, load, resize, scroll, unload, online, offline, hashchange, popstate
+// var btn = document.getElementById("btn");
+// btn.addEventListener("mouseover",function(){
+//     alert("Hello Again");
+// })
+// btn.addEventListener("mouseout",function(){
+//     alert("Hello Again");
+// })
+// var input = document.getElementById("input");
+// input.addEventListener("keydown",function(){
+//     alert("Hello Again");
+// })
+// input.addEventListener("keyup",function(){
+//     alert("Hello Again");
+// })
+// input.addEventListener("focus",function(){
+//     alert("Hello Again");
+// })
+// input.addEventListener("blur",function(){
+//     alert("Hello Again");
+// })
+// input.addEventListener("change",function(){
+//     alert("Hello Again");
+// })
+// var form = document.getElementById("form");
+// form.addEventListener("submit",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("load",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("resize",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("scroll",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("unload",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("online",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("offline",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("hashchange",function(){
+//     alert("Hello Again");
+// })
+// window.addEventListener("popstate",function(){
+//     alert("Hello Again");
+// })
