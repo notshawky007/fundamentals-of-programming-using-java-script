413d9ed6818ed1c534f7cbb7dc033cdc0dd15513
shawky_53 <mostafashawky100000@gmail.com>
2023-07-12 01:31:24 +0300

    practice of screen width and height

--- Bom/script.js
+++ Bom/script.js
@@ -1,16 +1,21 @@
-const scrollToTopButton = document.querySelector("#scroll-to-top");
+// const scrollToTopButton = document.querySelector("#scroll-to-top");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 400) {
-    scrollToTopButton.style.display = "block";
-  } else {
-    scrollToTopButton.style.display = "none";
-  }
-});
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 400) {
+//     scrollToTopButton.style.display = "block";
+//   } else {
+//     scrollToTopButton.style.display = "none";
+//   }
+// });
 
-scrollToTopButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
+// scrollToTopButton.addEventListener("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// });
+
+//practice of screen width and height
+// const width = document.documentElement.clientWidth;
+// const height = document.documentElement.clientHeight;
+// console.log(width, height);
